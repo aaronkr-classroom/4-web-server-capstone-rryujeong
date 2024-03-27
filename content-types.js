02f603e6ffe5a9986ba79c1847416f2a6e5000e7
rryujeong <126834727+rryujeong@users.noreply.github.com>
2024-03-27 06:20:46 +0000

    web app 03-27-2

---
+++ content-types.js
@@ -0,0 +1,25 @@
+// content-types.js
+"use strict";
+
+//Listing 7.4 (p.122)
+module.exports = {
+    html: {
+        "Content-Type": "text/html"
+    },
+    png: {
+        "Content-Type": "image/png"
+    },
+    jpg: {
+        "Content-Type": "image/jpg"
+    },
+    text: {
+        "Content-Type": "text/plain"
+    },
+    css: {
+        "Content-Type": "text/css"
+    },
+    js: {
+        "Content-Type": "text/javascript"
+    },
+};
+        
